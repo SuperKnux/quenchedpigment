5d51198e003e354f4c23a4b213d3d7b939d27e7b
Ski <34802236+SuperKnux@users.noreply.github.com>
2025-08-20 23:07:00 -0500

    Fixed BlockProvider

--- src/main/java/at/ski/quenchedpigment/datagen/QuenchedBlockModelsProvider.java
+++ src/main/java/at/ski/quenchedpigment/datagen/QuenchedBlockModelsProvider.java
@@ -2,23 +2,17 @@ package at.ski.quenchedpigment.datagen;
 
 import at.petrak.hexcasting.client.render.GaslightingTracker;
 import at.ski.quenchedpigment.block.QuenchedPigmentBlock;
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import kotlinx.serialization.json.Json;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.block.Block;
 import net.minecraft.data.client.*;
-import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import static at.ski.quenchedpigment.QuenchedPigment.MOD_ID;
@@ -32,20 +26,19 @@ public class QuenchedBlockModelsProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator generator) {
-        generatePigmentModels(QUENCHED_BLACK_BLOCK, generator, "black");
+        acceptBlockPigmentModel(QUENCHED_BLACK_BLOCK, generator, "black");
+        generateFourGaslightVariants(QUENCHED_BLACK_BLOCK, generator, "black");
     }
 
     @Override
-    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+    public void generateItemModels(ItemModelGenerator generator) {
 
-        itemModelGenerator.writer.accept(ModelIds.getItemModelId(QUENCHED_BLACK_BLOCK.asItem()), generateItemPigmentModels(ModelIds.getItemModelId(QUENCHED_BLACK_BLOCK.asItem()), "black"));
+        acceptItemPigmentModel(QUENCHED_BLACK_BLOCK, generator);
     }
 
-    public void generatePigmentModels(Block block, BlockStateModelGenerator generator, String color) {
-        generator.modelCollector.accept(ModelIds.getBlockModelId(block), generateBlockPigmentModel(ModelIds.getBlockModelId(block), color));
-    }
 
-    public Supplier<JsonElement> generateItemPigmentModels(Identifier identifier, String color) {
+
+    public Supplier<JsonElement> generateItemPigmentModels(Identifier identifier) {
         JsonObject jsonObject = new JsonObject();
         JsonArray jsonArray = new JsonArray();
 
@@ -54,7 +47,7 @@ public class QuenchedBlockModelsProvider extends FabricModelProvider {
             JsonObject jsonObject3 = new JsonObject();
 
 
-            jsonObject3.addProperty(GaslightingTracker.GASLIGHTING_PRED.toString(), i);
+            jsonObject3.addProperty(GaslightingTracker.GASLIGHTING_PRED.toString(), (double) i);
             jsonObject2.add("predicate", jsonObject3);
 
             jsonObject2.addProperty("model", identifier.toString() + "_" + i);
@@ -66,8 +59,22 @@ public class QuenchedBlockModelsProvider extends FabricModelProvider {
     }
 
     public Supplier<JsonElement> generateBlockPigmentModel(Identifier identifier, String color) {
-        JsonObject jsonObject = Models.CUBE_ALL.createJson(identifier, Map.of(TextureKey.ALL, Objects.requireNonNull(Identifier.of(MOD_ID, "quenched_" + color + "_block_0"))));
+        JsonObject jsonObject = Models.CUBE_ALL.createJson(identifier, Map.of(TextureKey.ALL, Objects.requireNonNull(Identifier.of(MOD_ID, "block/quenched_" + color + "_block_0"))));
 
         return () -> jsonObject;
+    }
+
+    public void acceptBlockPigmentModel(Block block, BlockStateModelGenerator generator, String color) {
+        generator.modelCollector.accept(ModelIds.getBlockModelId(block), generateBlockPigmentModel(ModelIds.getBlockModelId(block), color));
+    }
+
+    public void acceptItemPigmentModel(Block block, ItemModelGenerator generator){
+        generator.writer.accept(ModelIds.getItemModelId(block.asItem()), generateItemPigmentModels(ModelIds.getBlockModelId(block)));
+    }
+
+    public void generateFourGaslightVariants(Block block, BlockStateModelGenerator generator, String color) {
+        for (int i = 0; i < QuenchedPigmentBlock.VARIANTS; i++) {
+            generator.modelCollector.accept(Identifier.of(MOD_ID, block.toString() + "_" + i), generateBlockPigmentModel(ModelIds.getBlockModelId(block), color));
+        }
     }
 }
--- src/main/java/at/ski/quenchedpigment/datagen/QuenchedBlockStatesProvider.java
+++
@@ -1,4 +0,0 @@
-package at.ski.quenchedpigment.datagen;
-
-public class QuenchedBlockStatesProvider {
-}
